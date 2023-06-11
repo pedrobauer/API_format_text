067f0a8b325ca71f87bb4c933bee12b583c5981d
Pedro Henrique Lopes Bauer <80714241+pedrobauer@users.noreply.github.com>
2023-06-11 20:37:45 -0300

    EndPoint tudo_minusculo Finalizado

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const { contadorDePalavras } = require('./controllers/contadorDePalavra');
 const { tudoMaiusculo } = require('./controllers/tudoMaiuscula');
+const { tudoMinusculo } = require('./controllers/tudoMinusculo');
 
 const routes = express();
 
@@ -10,5 +11,7 @@ routes.get('/', (req, res) => { res.send('tudo ok') });
 routes.get('/contador', contadorDePalavras);
 
 routes.get('/maiusculo', tudoMaiusculo);
+
+routes.get('/minusculo', tudoMinusculo);
 
 module.exports = routes;
